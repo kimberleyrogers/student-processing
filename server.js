c22e684fa70c72c2175c61f48999889f174d0150
Kimberley Rogers <kimberleyrogers@live.co.uk>
2022-07-28 19:07:02 +1000

    added server files

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const pg = require('pg')
+
+// const port = 3000;
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('hello')
+});
+
+app.listen(port, () => {
+  console.log(`server listening on port: ${port}`)
+});
