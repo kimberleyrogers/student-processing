917af23fc9978a6fad4a28590edb9a5a80515941
Kimberley Rogers <kimberleyrogers@live.co.uk>
2022-08-05 17:14:00 +1000

    changes to imports

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,13 +2,6 @@ import { render, screen } from '@testing-library/react';
 import { App, AboutPage } from './App';
 import { Login } from './login';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
-
-
 test('aboutPage Route displays with created by content', () => {
   render(<AboutPage />);
   const heading = screen.getByRole('heading', {name: "created by Kimberley Rogers"});
